ef3d2e1489060583d4bdcc2f196a70383f51384f
Semicolon ITES (Developer) <semicolonites-developer@Semicolons-Mini.lan>
2025-05-17 19:27:02 +0530

    gtm added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,37 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+      {/* <!-- Google Tag Manager --> */}
+      <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N5NJBXRK');
+        `}
+        </Script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7P1QPY7F1"/>
+        <Script>
+          {
+            ` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-Z7P1QPY7F1');`
+          }
+        </Script>
+      </head>
+      {/* <!-- End Google Tag Manager --> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N5NJBXRK"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         {children}
       </body>
     </html>
