71bf208980caaecef3099b886fd590feb20305cd
Semicolon ITES (Developer) <semicolonites-developer@Semicolons-Mini.lan>
2025-05-18 10:36:14 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,26 +22,37 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       {/* <!-- Google Tag Manager --> */}
+
       <head>
+        {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-N5NJBXRK');
-        `}
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-N5NJBXRK');
+    `}
         </Script>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z7P1QPY7F1"/>
-        <Script>
-          {
-            ` window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-Z7P1QPY7F1');`
-          }
+
+        {/* Google Analytics Script Loader */}
+        <Script
+          id="ga-loader-script"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z7P1QPY7F1"
+        />
+
+        {/* Google Analytics Config Script */}
+        <Script id="ga-config-script" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-Z7P1QPY7F1');
+    `}
         </Script>
       </head>
+
       {/* <!-- End Google Tag Manager --> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
